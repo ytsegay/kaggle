56a48b6cfa742cbe0fccfb37dd1fe4ad8ceb9d37
Yohannes Tsegay <ytsegay@ebay.com>
2014-08-10 17:12:06 -0700

    Stable code with numeric and text feats
    Final stable stage with numeric and textual features disjoint

--- russianProhibContent/generateModel.py
+++ russianProhibContent/generateModel.py
@@ -1,43 +1,133 @@
 import pickle
 import random
 from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.feature_extraction.text import HashingVectorizer
 import sklearn.linear_model as lm
 from sklearn import cross_validation
-#from sklearn.ensemble import RandomForestClassifier
-from sklearn.ensemble import GradientBoostingClassifier
+from sklearn.ensemble import RandomForestClassifier,GradientBoostingClassifier
+from sklearn.svm import LinearSVC
 from sklearn.metrics import precision_score
 from sklearn.metrics import classification_report
 from sklearn.cross_validation import train_test_split
+from sklearn.naive_bayes import BernoulliNB, MultinomialNB
+from sklearn.linear_model import SGDClassifier
+
 import numpy
 import time
+from sklearn.preprocessing import OneHotEncoder
 
 
-def targetMapper(str):
+def targetMapper(str, isProvedIlicit=u'0'):
     if str == u'1':
+        if isProvedIlicit == u'1':
+            return 2
         return 1
     return 0
 
-def sampleTextFields(rows, sampleSize, vec, target, numericFeats, ids, hasTarget=True):
+def cleanUpData(rows, sampleSize, features, hasTarget=True):
+
+    countofApproved = {}
+    durationOfAdd = []
+
     sample = random.sample(xrange(len(rows)), sampleSize)
     for index in sample:
         row = rows[index]
-        text = row['title'] + " " + row['description'] + " " + row['category'] + " " + row['subcategory'] + " " + row['attrs']
-        vec.append(text)
-        ids.append(row["itemid"]);
 
+        #ad id
+        features["ids"].append(row["itemid"])
+
+        ###### textual work
+        # concat attribute keys and valus into a single string
+        attrs = ''
+        for keyz in row['attrs']:
+            attrs += " " + keyz + " " + row['attrs'][keyz]
+        attrs = attrs.strip()
+
+        # textual parts will be the title description and attributes
+        #
+        text = row['title'] + " " + row['description'] + " " + attrs + " "
+        features["textDescription"].append(text)
+
+
+        ###### nuermic features
         arr = []
-        arr.append(row['price'])
-        arr.append(row['phones_cnt'])
-        arr.append(row['emails_cnt'])
-        arr.append(row['urls_cnt'])
-        #titleLength = len(row['title'].split())
-        #textLength = len(text.split())
-        #arr.append(titleLength)
-        #arr.append(textLength)
-        numericFeats.append(arr)
+
+        # does the list contain a phone #?
+        hasPhone = 0
+        if (int(row['phones_cnt']) > 0):
+            hasPhone = 1
+        arr.append(hasPhone)
+
+        # boolean if it has an email
+        hasEmail = 0
+        if (int(row['emails_cnt']) > 0):
+            hasEmail = 1
+        arr.append(hasEmail)
+
+        # boolean attribute if it has a URL
+        hasUrls = 0
+        if (int(row['urls_cnt']) > 0):
+            hasUrls = 1
+        arr.append(hasUrls)
+
+        # encode category and sub category
+        entry = row['subcategory']
+        if entry not in features["lookupSubCats"]:
+            features["lookupSubCats"][entry] = len(features["lookupSubCats"])
+        arr.append(features["lookupSubCats"][entry])
+
+        entry = row['category']
+        if entry not in features["lookupCats"]:
+            features["lookupCats"][entry] = len(features["lookupCats"])
+        arr.append(features["lookupCats"][entry])
+
+
+        arr.append(float(row['price']))
+
+        # additional feature count words in title
+        titleLength = len(row['title'].split())
+        arr.append(titleLength)
+
+        # count of words in the description
+        textLength = len(text.split())
+        arr.append(textLength)
+
+        # depth of the attributes
+        arr.append(len(row['attrs']))
+
+
 
         if hasTarget == True:
-            target.append(targetMapper(row['is_blocked']))
+            features["target"].append(targetMapper(row['is_blocked']))
+
+        features["numericDescription"].append(arr)
+
+        if row['is_proved'] not in countofApproved:
+            countofApproved[row['is_proved']] = 0
+        countofApproved[row['is_proved']] += 1
+
+        if row['close_hours'].strip() != '':
+            durationOfAdd.append(float(row['close_hours'])*60)
+
+
+
+
+    print countofApproved
+    durationOfAdd = numpy.array(durationOfAdd)
+    print "duration mean: ", numpy.mean(durationOfAdd)
+    print "duration stddev: ", numpy.std(durationOfAdd)
+    print "duration median: ", numpy.median(durationOfAdd)
+
+
+def encodeCategoricalStringFeats(rows):
+    lookUp = {}
+    arr = []
+    for entry in rows:
+        if entry not in lookUp:
+            lookUp[entry] = len(lookUp)
+        arr.append(lookUp[entry])
+    return numpy.array(arr)
+
 
 def loadPickledFile(fileName):
     fl = open(fileName, 'rb')
@@ -51,119 +141,130 @@ if __name__=="__main__":
     t1 = time.time()
     print "unpicked blocked, took ",(t1-t0)
 
+
     t0 = time.time()
     unblocked = loadPickledFile('output/trainunblocked.pkl')
     t1 = time.time()
     print "unpicked unblocked, took ",(t1-t0)
 
     sampleSize = len(blocked)
-    vec = []
-    target = []
-    docIds = []
-    numerifFeats = []
+    rows = {}
+
+    rows["textDescription"] = [];
+    rows["numericDescription"] = [];
+    rows["categoryDescription"] = [];
+    rows["subcategoryDescription"] = []
+    rows["ids"] = []
+    rows["target"] = []
+    rows["lookupCats"] = {}
+    rows["lookupSubCats"] = {}
 
     t0 = time.time()
-    sampleTextFields(blocked, sampleSize, vec, target, numerifFeats, docIds)
-    sampleTextFields(unblocked, sampleSize, vec, target, numerifFeats, docIds)
+    cleanUpData(blocked, sampleSize, rows)
+    cleanUpData(unblocked, sampleSize, rows)
     t1 = time.time()
-    print "Done sampling, took ", (t1-t0), " Sample size: ", len(vec)
+    print "Done sampling, took ", (t1-t0), " Sample size: ", sampleSize
 
     del blocked
     del unblocked
 
-    t0 = time.time()
-    wordVectorizer = TfidfVectorizer(ngram_range=(1,1), analyzer="word", binary=False, min_df=2, smooth_idf=True, sublinear_tf=True, use_idf=True, max_features=50000, max_df=0.8, lowercase=True)
-    trainTfIdf = wordVectorizer.fit_transform(vec)
-    #charVectorizer = TfidfVectorizer(ngram_range=(1,5), analyzer="char", binary=False, min_df=2, smooth_idf=True, sublinear_tf=True, use_idf=True, max_features=50000, max_df=0.8, lowercase=True)
-    #charTrainTfIdf = charVectorizer.fit_transform(vec)
-    t1 = time.time()
-    print "vectorized. Took ", (t1-t0)
-
-
-    t1 = time.time()
-    print "Done learning, ", (t1-t0)
-    #print scores
+#    t0 = time.time()
+#    wordVectorizer = TfidfVectorizer(ngram_range=(1,1), analyzer="word", binary=False, min_df=2, smooth_idf=True, sublinear_tf=True, use_idf=True, max_features=50000, max_df=0.9, lowercase=True)
+#    trainTfIdf = wordVectorizer.fit_transform(rows["textDescription"])
+#    t1 = time.time()
+#    print "vectorized. Took ", (t1-t0)
 
     #### Testing
-    testing = True
+    testing = False
     if testing:
         testVec = []
-        testIds = []
-        testTargets = []
-        numericFeatures = []
-
-
-        clf = lm.LogisticRegression(penalty='l1', dual=False, tol=1e-06, C=3, fit_intercept=True,intercept_scaling=0.1)
-        clf = clf.fit(trainTfIdf, numpy.array(target))
-
-        t0 = time.time()
-        testData = loadPickledFile('output/testUnblocked.pkl')
-        t1 = time.time()
-        print "unpicked test file, took ",(t1-t0)
-
-        sampleTextFields(testData, len(testData), testVec, testTargets, numericFeatures, testIds, False)
-        testTfIdfs = wordVectorizer.transform(testVec)
-        predics_probas = clf.predict_proba(testTfIdfs)
-
-        zippedProbaWeight = sorted(zip(predics_probas[:,1], testIds), reverse=True)
-
-        f = open('output/predict2.txt','w')
-        for entry in zippedProbaWeight:
-            line = str(entry[1]) + "\n"
-            f.write(line)
-        f.close()
+#        testIds = []
+#        testTargets = []
+#        numericFeatures = []
+#
+#        target = numpy.array(target)
+#        numericFeats = numpy.array(numericFeats, dtype=float)
+#
+#        clf = lm.LogisticRegression(penalty='l1', dual=False, tol=1e-06, C=3, fit_intercept=True,intercept_scaling=0.1)
+#        clf = clf.fit(trainTfIdf, target)
+#
+#        nonencodable = numericFeats[:,[5,6,7,8]]
+#        enc = OneHotEncoder()
+#        hotEncoded = numpy.array(enc.fit_transform(numericFeats[:,[0,1,2,3,4]]).toarray().tolist())
+#        newFeats = numpy.concatenate((hotEncoded, nonencodable), axis=1)
+#
+#        clfNum = GradientBoostingClassifier(n_estimators=500, random_state=0)
+#        clfNum = clfNum.fit(newFeats, target)
+#
+#        t0 = time.time()
+#        testData = loadPickledFile('output/testUnblocked.pkl')
+#        t1 = time.time()
+#        print "unpicked test file, took ",(t1-t0)
+#
+#        sampleTextFields(testData, len(testData), testVec, testTargets, numericFeatures, testIds, categoryLookup, subCatLookup, False)
+#        testTfIdfs = wordVectorizer.transform(testVec)
+#
+#        numericFeatures = numpy.array(numericFeatures)
+#        nonencodableTest = numericFeatures[:,[5,6,7,8]]
+#        hotEncodedTest = numpy.array(enc.fit_transform(numericFeatures[:,[0,1,2,3,4]]).toarray().tolist())
+#        newFeatsTest = numpy.concatenate((hotEncodedTest, nonencodableTest), axis=1)
+#
+#        numerif_probas = clfNum.predict_proba(newFeatsTest)[:,1]
+#
+#        predics_probas = clf.predict_proba(testTfIdfs)[:,1]
+#        zippedWeights = zip(predics_probas, numerif_probas)
+#        sumed = [(entry[0]*0.8 + entry[1]*0.2) for entry in zippedWeights]
+#
+#        zippedProbaWeight = sorted(zip(sumed, testIds), reverse=True)
+#
+#        f = open('output/predict3_0.8.txt','w')
+#        for entry in zippedProbaWeight:
+#            line = str(entry[1]) + "\n"
+#            f.write(line)
+#        f.close()
     else:
         t0 = time.time()
 
-        target = numpy.array(target)
-        numerifFeats = numpy.array(numerifFeats, dtype=int)
+        target = numpy.array(rows["target"])
+        numericFeats = numpy.array(rows["numericDescription"], dtype=float)
+        rows["textDescription"] = numpy.array(rows["textDescription"])
 
-#        #paramter grid search
-#        tunedParams = [{
-#            'penalty': ['l1'],
-#            'tol': [1e-5, 1e-8, 1e-10],
-#            'C': [1, 2, 3, 4],
-#            #'fit_intercept': [True, False]
-#            }]
-#
-#        X_train, X_test, y_train, y_test = train_test_split(numerifFeats, target, test_size=0.10, random_state=0)
-#
-#        clf = GridSearchCV(GradientBoostingClassifier(verbose=1), tuned_parameters, scoring='precision', cv=3, n_jobs=1, verbose=10)
-#        #clf = GridSearchCV(lm.LogisticRegression(dual=False, intercept_scaling=1.0), tunedParams, cv=3, scoring='precision', n_jobs=2, verbose=5)
-#        clf.fit(X_train, y_train)
-#        print "Best parameters set found on development set:"
-#        print ""
-#        print clf.best_estimator_
-#        print "Grid scores on development set:"
-#
-#        for params, mean_score, scores in clf.grid_scores_:
-#            print("%0.3f (+/-%0.03f) for %r"
-#            % (mean_score, scores.std() / 2, params))
-#            print ""
-#        print "Detailed classification report:"
-#        print ""
-#        print "The model is trained on the full development set."
-#        print "The scores are computed on the full evaluation set."
-#        print ""
-#        y_true, y_pred = y_test, clf.predict(X_test)
-#        print classification_report(y_true, y_pred)
-#        print ""
-#
+        rows["categoryDescription"] = encodeCategoricalStringFeats(rows["categoryDescription"])
+        rows["subcategoryDescription"] = encodeCategoricalStringFeats(rows["subcategoryDescription"])
 
         scores = []
         # split into train and test
         rs = cross_validation.ShuffleSplit(len(target), 5, test_size=0.2)
         for trainIndex, testIndex in rs:
+            wordVectorizer = TfidfVectorizer(ngram_range=(1,1), analyzer="word", binary=False, min_df=2, smooth_idf=True, sublinear_tf=True, use_idf=True, max_features=50000, max_df=0.9, lowercase=True)
+            trainTfIdf = wordVectorizer.fit_transform(rows["textDescription"][trainIndex])
+            testTfIdf = wordVectorizer.transform(rows["textDescription"][testIndex])
+            t1 = time.time()
+            print "vectorized. Took ", (t1-t0)
 
+            print testTfIdf[-1]
+            # text features
             clf = lm.LogisticRegression(penalty='l1', dual=False, tol=0.0000001, C=3, fit_intercept=True,intercept_scaling=0.1)
-            clfNum = lm.LogisticRegression(penalty='l1', dual=False, tol=0.0000001, C=3, fit_intercept=True,intercept_scaling=1.0)
+            clf = clf.fit(trainTfIdf, target[trainIndex])
+            wordsTestPredict = clf.predict_proba(testTfIdf)[:,1]
+            stacked = zip(wordsTestPredict, wordsTestPredict)
 
-            wordPredict = clf.fit(trainTfIdf[trainIndex,:], target[trainIndex]).predict_proba(trainTfIdf[testIndex,:])[:,1]
-            numericFeatsPredict = clfNum.fit(numerifFeats[trainIndex,:], target[trainIndex]).predict_proba(numerifFeats[testIndex,:])[:,1]
+#            # **** numeric features
+             # one hot encode all but the price
+            nonencodable = numericFeats[:,[5,6,7,8]]
+            enc = OneHotEncoder()
+            hotEncoded = numpy.array(enc.fit_transform(numericFeats[:,[0,1,2,3,4]]).toarray().tolist())
+            newFeats = numpy.concatenate((hotEncoded, nonencodable), axis=1)
+#
+            clfNum = lm.LogisticRegression(penalty='l1', dual=False, tol=0.0000001, C=3, fit_intercept=True,intercept_scaling=0.1) #GradientBoostingClassifier(n_estimators=100, random_state=0)
+            numericFeatsPredict = clfNum.fit(newFeats[trainIndex,:], target[trainIndex]).predict_proba(newFeats[testIndex,:])[:,1]
 
+            stacked = zip(numericFeatsPredict, numericFeatsPredict)
             #stacked = zip(wordPredict, numericFeatsPredict)
-            stacked = zip(wordPredict, wordPredict)
-            sumed = [round((entry[0]*0.8 + entry[1]*0.2),0) for entry in stacked]
+            #stacked = zip(wordPredict, wordPredict)
+
+            #sumed = [1 if ((entry[0]*0.8 + entry[1]*0.2) >= 0.5) else 0 for entry in stacked]
+            sumed = [numpy.round(entry[0]) for entry in stacked]
             score = precision_score(target[testIndex], sumed)
             scores.append(score)
             print "Learned ...", score
--- russianProhibContent/myParser.py
+++ russianProhibContent/myParser.py
@@ -6,135 +6,147 @@ import random
 import pickle
 
 def populateRussianStopWords():
-	f = open('data/stopwords_ru.txt', 'rb')
-	lines = f.readlines()
-	f.close()
-	#arr = [word.decode('utf-8') for word in nltk.corpus.stopwords.words("russian") if word != "He"]
-	arr = [word.strip().decode('utf-8') for word in lines]
+    f = open('data/stopwords_ru.txt', 'rb')
+    lines = f.readlines()
+    f.close()
+    #arr = [word.decode('utf-8') for word in nltk.corpus.stopwords.words("russian") if word != "He"]
+    arr = [word.strip().decode('utf-8') for word in lines]
 
-	return arr
+    return arr
 
 stopwords = populateRussianStopWords()
 stemmer = SnowballStemmer('russian')
 
 
 def stem_word(str, shouldStem):
-	if shouldStem == True:
-		return stemmer.stem(str)
-	return str
+    if shouldStem == True:
+        return stemmer.stem(str)
+    return str
 
 def stop_word(str, shouldStop):
-	if (shouldStop):
-		if str in stopwords:
-			return ""
+    if (shouldStop):
+        if str in stopwords:
+            return ""
 
-	return str
+    return str
 
 def translate_word(w):
-	cyrillic_translit={u'\u0410': 'A', u'\u0430': 'a',
-		u'\u0411': 'B', u'\u0431': 'b',
-		u'\u0412': 'V', u'\u0432': 'v',
-		u'\u0413': 'G', u'\u0433': 'g',
-		u'\u0414': 'D', u'\u0434': 'd',
-		u'\u0415': 'E', u'\u0435': 'e',
-		u'\u0416': 'Zh', u'\u0436': 'zh',
-		u'\u0417': 'Z', u'\u0437': 'z',
-		u'\u0418': 'I', u'\u0438': 'i',
-		u'\u0419': 'I', u'\u0439': 'i',
-		u'\u041a': 'K', u'\u043a': 'k',
-		u'\u041b': 'L', u'\u043b': 'l',
-		u'\u041c': 'M', u'\u043c': 'm',
-		u'\u041d': 'N', u'\u043d': 'n',
-		u'\u041e': 'O', u'\u043e': 'o',
-		u'\u041f': 'P', u'\u043f': 'p',
-		u'\u0420': 'R', u'\u0440': 'r',
-		u'\u0421': 'S', u'\u0441': 's',
-		u'\u0422': 'T', u'\u0442': 't',
-		u'\u0423': 'U', u'\u0443': 'u',
-		u'\u0424': 'F', u'\u0444': 'f',
-		u'\u0425': 'Kh', u'\u0445': 'kh',
-		u'\u0426': 'Ts', u'\u0446': 'ts',
-		u'\u0427': 'Ch', u'\u0447': 'ch',
-		u'\u0428': 'Sh', u'\u0448': 'sh',
-		u'\u0429': 'Shch', u'\u0449': 'shch',
-		u'\u042a': '"', u'\u044a': '"',
-		u'\u042b': 'Y', u'\u044b': 'y',
-		u'\u042c': "'", u'\u044c': "'",
-		u'\u042d': 'E', u'\u044d': 'e',
-		u'\u042e': 'Iu', u'\u044e': 'iu',
-		u'\u042f': 'Ia', u'\u044f': 'ia'}
+    cyrillic_translit={u'\u0410': 'A', u'\u0430': 'a',
+        u'\u0411': 'B', u'\u0431': 'b',
+        u'\u0412': 'V', u'\u0432': 'v',
+        u'\u0413': 'G', u'\u0433': 'g',
+        u'\u0414': 'D', u'\u0434': 'd',
+        u'\u0415': 'E', u'\u0435': 'e',
+        u'\u0416': 'Zh', u'\u0436': 'zh',
+        u'\u0417': 'Z', u'\u0437': 'z',
+        u'\u0418': 'I', u'\u0438': 'i',
+        u'\u0419': 'I', u'\u0439': 'i',
+        u'\u041a': 'K', u'\u043a': 'k',
+        u'\u041b': 'L', u'\u043b': 'l',
+        u'\u041c': 'M', u'\u043c': 'm',
+        u'\u041d': 'N', u'\u043d': 'n',
+        u'\u041e': 'O', u'\u043e': 'o',
+        u'\u041f': 'P', u'\u043f': 'p',
+        u'\u0420': 'R', u'\u0440': 'r',
+        u'\u0421': 'S', u'\u0441': 's',
+        u'\u0422': 'T', u'\u0442': 't',
+        u'\u0423': 'U', u'\u0443': 'u',
+        u'\u0424': 'F', u'\u0444': 'f',
+        u'\u0425': 'Kh', u'\u0445': 'kh',
+        u'\u0426': 'Ts', u'\u0446': 'ts',
+        u'\u0427': 'Ch', u'\u0447': 'ch',
+        u'\u0428': 'Sh', u'\u0448': 'sh',
+        u'\u0429': 'Shch', u'\u0449': 'shch',
+        u'\u042a': '"', u'\u044a': '"',
+        u'\u042b': 'Y', u'\u044b': 'y',
+        u'\u042c': "'", u'\u044c': "'",
+        u'\u042d': 'E', u'\u044d': 'e',
+        u'\u042e': 'Iu', u'\u044e': 'iu',
+        u'\u042f': 'Ia', u'\u044f': 'ia'}
 
-	converted_word = ''
-	for char in w:
-		transchar = ''
-		if char in cyrillic_translit:
-			transchar = cyrillic_translit[char]
-		else:
-			transchar = char
-		converted_word += transchar
-	return converted_word
+    converted_word = ''
+    for char in w:
+        transchar = ''
+        if char in cyrillic_translit:
+            transchar = cyrillic_translit[char]
+        else:
+            transchar = char
+        converted_word += transchar
+    return converted_word
 
 def normalize_words(words, shouldStem, shouldStop):
-	retStr = ""
-	for word in words:
-		word = stop_word(word, shouldStop)
-		word = stem_word(word, shouldStem)
+    retStr = ""
+    for word in words:
+        word = stop_word(word, shouldStop)
+        word = stem_word(word, shouldStem)
 
-		if (word.strip() != ""):
-			retStr += translate_word(word.strip()) + " "
-	return retStr.strip()
+        if (word.strip() != ""):
+            retStr += translate_word(word.strip()) + " "
+    return retStr.strip()
 
 def load_data(fileName, linesCount, columnsToUse):
-	
-	cleanedDataBlocked = []
-	cleanedDataNotBlocked = []
-	countPos = 0
-	total = 0
-	
-	tokenizer = RegexpTokenizer(r'\w+')
 
-	linesCounter = 0
-	with open(fileName,'rb') as fl:
-		itemReader = csv.DictReader(fl, delimiter='\t', quotechar = '"')
-		for i, item in enumerate(itemReader):
-			if linesCounter >= 0:
-				item = {featureName: featureValue.decode('utf-8') for featureName,featureValue in item.iteritems()}
-				for columnToUse in columnsToUse:
-					words = tokenizer.tokenize(item[columnToUse])
-					item[columnToUse] = normalize_words(words, True, True)
+    cleanedDataBlocked = []
+    cleanedDataNotBlocked = []
+    countPos = 0
+    total = 0
 
-				
-				if 'is_blocked' in item and item['is_blocked'] == u'1':
-					countPos += 1
-					cleanedDataBlocked.append(item)
-				else:
-					cleanedDataNotBlocked.append(item)
-				total += 1
-			linesCounter += 1
-			if ((linesCounter % 10000) == 0):
-				print linesCounter,"\r"
-	return cleanedDataNotBlocked,cleanedDataBlocked
+    tokenizer = RegexpTokenizer(r'\w+')
 
-	
+    linesCounter = 0
+    with open(fileName,'rb') as fl:
+        itemReader = csv.DictReader(fl, delimiter='\t', quotechar = '"')
+        for i, item in enumerate(itemReader):
+            if linesCounter >= 0:
+                item = {featureName: featureValue.decode('utf-8') for featureName,featureValue in item.iteritems()}
+                for columnToUse in columnsToUse:
+
+                    if columnToUse == 'attrs':
+                        ## convert attributes to key value pairs
+                        arr = {}
+                        a = item[columnToUse].split('", "')
+                        for e in a:
+                            p = e.split('":"')
+                            if (len(p) == 2):
+                                arr[normalize_words([p[0]], True, True)] = normalize_words([p[1]], False, True)
+                        item[columnToUse] = arr
+                        #print item[columnToUse]
+                    else:
+                        words = tokenizer.tokenize(item[columnToUse])
+                        item[columnToUse] = normalize_words(words, False, True)
+
+
+                if 'is_blocked' in item and item['is_blocked'] == u'1':
+                    countPos += 1
+                    cleanedDataBlocked.append(item)
+                else:
+                    cleanedDataNotBlocked.append(item)
+                total += 1
+            linesCounter += 1
+            if ((linesCounter % 10000) == 0):
+                print linesCounter,"\r"
+    return cleanedDataNotBlocked,cleanedDataBlocked
+
+
 
 if __name__=="__main__":
-	train = False;
-	if train == True:
-		outputUnBlocked, outputBlocked = load_data("data/train.tsv", 100000, ['category', 'subcategory', 'title', 'description', 'attrs'])
-	
-		fklFl = open("output/trainunblocked.pkl", "wb")
-		pickle.dump(outputUnBlocked, fklFl);
-		fklFl.close()
-		
-		fklFl = open("output/trainblocked.pkl", "wb")
-		pickle.dump(outputBlocked, fklFl);
-		fklFl.close()
-	else:
-		outputUnBlocked, outputBlocked = load_data("data/test.tsv", 100000, ['category', 'subcategory', 'title', 'description', 'attrs'])
-		fklFl = open("output/testUnblocked.pkl", "wb")
-		pickle.dump(outputUnBlocked, fklFl);
-		fklFl.close()
-		
-		fklFl = open("output/testBlocked.pkl", "wb")
-		pickle.dump(outputBlocked, fklFl);
-		fklFl.close()
+    train = False;
+    if train == True:
+        outputUnBlocked, outputBlocked = load_data("data/train.tsv", 100000, ['category', 'subcategory', 'title', 'description', 'attrs'])
+
+        fklFl = open("output/trainunblocked.pkl", "wb")
+        pickle.dump(outputUnBlocked, fklFl);
+        fklFl.close()
+
+        fklFl = open("output/trainblocked.pkl", "wb")
+        pickle.dump(outputBlocked, fklFl);
+        fklFl.close()
+    else:
+        outputUnBlocked, outputBlocked = load_data("data/test.tsv", 100000, ['category', 'subcategory', 'title', 'description', 'attrs'])
+        fklFl = open("output/testUnblocked.pkl", "wb")
+        pickle.dump(outputUnBlocked, fklFl);
+        fklFl.close()
+
+        fklFl = open("output/testBlocked.pkl", "wb")
+        pickle.dump(outputBlocked, fklFl);
+        fklFl.close()
